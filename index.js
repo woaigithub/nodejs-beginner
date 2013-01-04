9a396540b1a579d9dd8d7eded2c87ee6a9bd3067
woaigithub <jorden008@gmail.com>
2013-01-04 23:18:54 +0800

    simple router

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var server= require("./server.js");
+var router=require("./router.js");
 
-server.begin();
+server.begin(router.route);
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http=require("http");
+var url= require("url");
 
-function start(){
+function start(route){
 
   function onRequest(request, response){
-    console.log(request.url);
+    var pathname=url.parse(request.url).pathname;
+    console.log("Request for "+pathname+" received.");
+
+    route(pathname);
     console.log(request.headers["user-agent"]);
-    console.log("request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello, world!");
     response.end();
