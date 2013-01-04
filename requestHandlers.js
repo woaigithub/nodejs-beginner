efa0149ce398b84d6ebc07ad9ed0f7b5136d4ba8
woaigithub <jorden008@gmail.com>
2013-01-04 23:57:05 +0800

    upgrade reqeustHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,8 @@
+function sleep(milliSeconds){
+  var startTime=new Date().getTime();
+  while(new Date().getTime() < startTime + milliSeconds);
+}
+
 function home(){
   console.log("request handler 'home' was called.");
   return "hello, world";
@@ -5,6 +10,9 @@ function home(){
 
 function start(){
   console.log("request handler 'start' was called.");
+  
+  sleep(10000);
+
   return "hello, start";
 }
 
